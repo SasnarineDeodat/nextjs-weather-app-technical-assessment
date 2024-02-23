986ca27a9feb2bb7efc7070d2e590ccec9160b10
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-23 18:47:20 -0500

    created the heading of the homepage

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="flex justify-center items-center text-center h-screen">
+      <h1 className="text-3xl">Get Weather Details</h1>
     </div>
   );
 }
