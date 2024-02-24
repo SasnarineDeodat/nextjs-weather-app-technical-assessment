b761a675adfd2128581e5c4e744c599e1d71168b
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-23 19:09:59 -0500

    aligned the WeatherCard and MiniCard components

--- app/weather/page.js
+++ app/weather/page.js
@@ -12,29 +12,30 @@ export default function Page() {
   const { weather, thisLocation, values, place, setPlace } = useStateContext();
   return (
     <div>
-      Weather
       <BackgroundLayout></BackgroundLayout>
-      <WeatherCard
-        place={thisLocation}
-        windspeed={weather.wspd}
-        humidity={weather.humidity}
-        temperature={weather.temp}
-        heatIndex={weather.heatindex}
-        iconString={weather.conditions}
-        conditions={weather.conditions}
-      />
-      <div className="flex justify-center gap-8 flex-wrap w-[60%]">
-        {values?.slice(1, 7).map((curr) => {
-          return (
-            <MiniCard
-              key={curr.datetime}
-              time={curr.datetime}
-              temp={curr.temp}
-              iconString={curr.conditions}
-            />
-          );
-        })}
-      </div>
+      <main className="w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center">
+        <WeatherCard
+          place={thisLocation}
+          windspeed={weather.wspd}
+          humidity={weather.humidity}
+          temperature={weather.temp}
+          heatIndex={weather.heatindex}
+          iconString={weather.conditions}
+          conditions={weather.conditions}
+        />
+        <div className="flex justify-center gap-8 flex-wrap w-[60%]">
+          {values?.slice(1, 7).map((curr) => {
+            return (
+              <MiniCard
+                key={curr.datetime}
+                time={curr.datetime}
+                temp={curr.temp}
+                iconString={curr.conditions}
+              />
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
